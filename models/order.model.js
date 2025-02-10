099d2f1a676892b49835f8ee7856f485bceff051
MfatihC02 <139883287+MfatihC02@users.noreply.github.com>
2025-02-10 12:00:49 +0300

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -33,9 +33,11 @@ const OrderItemSchema = new Schema({
 // Sipariş numarası oluşturma yardımcı fonksiyonu
 async function generateOrderNumber() {
     const date = new Date();
-    const formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`;
-    const uuid = uuidv4();
-    const orderNumber = `${formattedDate}-${uuid}`;
+    const year = date.getFullYear().toString().slice(-2);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const randomNum = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+    const orderNumber = `${year}${month}${day}-${randomNum}`;
     return orderNumber;
 }
 
